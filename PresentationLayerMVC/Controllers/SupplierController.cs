60f4397bc13e66e49e2fd8cbd78dc5dc4808cc6c
Bruno Felipe Alves <bruno.felipe_alves@hotmail.com>
2021-03-14 03:27:50 -0300

    Up

---
+++ PresentationLayerMVC/Controllers/SupplierController.cs
@@ -0,0 +1,149 @@
+﻿using AutoMapper;
+using BusinessLogicalLayer.Interfaces;
+using Common;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PresentationLayerMVC.Models;
+using PresentationLayerMVC.Models.CompanyModels;
+using PresentationLayerMVC.Models.SupplierModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayerMVC.Controllers
+{
+    public class SupplierController : Controller
+    {
+        private readonly IMapper _mapper;
+
+        public SupplierController(IMapper mapper)
+        {
+            this._mapper = mapper;
+
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+
+                HttpResponseMessage responseMessage = await client.GetAsync(Startup.UrlBase + "CompanyAPI");
+                string jsonResponse = await responseMessage.Content.ReadAsStringAsync();
+                QueryResponse<Company> response = JsonConvert.DeserializeObject<QueryResponse<Company>>(jsonResponse);
+                if (!response.Success)
+                {
+                    return RedirectToAction("Index", "Company");
+                }
+                ViewBag.Companies = response.Data.Select(r => new SelectListItem { Value = r.ID.ToString(), Text = r.CommercialName }).ToList();
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(SupplierCpfInsertViewModel viewModel)
+        {
+            Supplier supplier = _mapper.Map<Supplier>(viewModel);
+
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(supplier), Encoding.UTF8, "application/json");
+                HttpResponseMessage responseMessage = await client.PostAsync(Startup.UrlBase + "SupplierAPI", content);
+                string jsonResponse = await responseMessage.Content.ReadAsStringAsync();
+                Response response = JsonConvert.DeserializeObject<Response>(jsonResponse);
+                if (response.Success)
+                {
+                    return RedirectToAction("Index");
+                }
+                ViewBag.Erros = response.Message;
+                return View();
+            }
+        }
+        //-----------------------------------------------------
+        [HttpPost]
+        public async Task<IActionResult> Edit(CompanyUpdateViewModel viewModel)
+        {
+            Company company = _mapper.Map<Company>(viewModel);
+
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(company), Encoding.UTF8, "application/json");
+                HttpResponseMessage responseMessage = await client.PostAsync(Startup.UrlBase + "CompanyAPI/Update", content);
+                string jsonResponse = await responseMessage.Content.ReadAsStringAsync();
+                Response response = JsonConvert.DeserializeObject<Response>(jsonResponse);
+                if (response.Success)
+                {
+                    return RedirectToAction("Index");
+                }
+                ViewBag.Erros = response.Message;
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(Startup.UrlBase + "CompanyAPI/Detail/?id=" + id);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    string companyJsonResponse = await response.Content.ReadAsStringAsync();
+                    SingleResponse<Company> companyResponse = JsonConvert.DeserializeObject<SingleResponse<Company>>(companyJsonResponse);
+                    CompanyQueryViewModel viewModel = _mapper.Map<CompanyQueryViewModel>(companyResponse.Data);
+                    return View(_mapper.Map<CompanyUpdateViewModel>(viewModel));
+                }
+                ViewBag.Errors = "Dados não encontrados.";
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(Startup.UrlBase + "CompanyAPI/GetActives");
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    string companyJsonResponse = await response.Content.ReadAsStringAsync();
+                    QueryResponse<Company> companyResponse = JsonConvert.DeserializeObject<QueryResponse<Company>>(companyJsonResponse);
+                    return View(_mapper.Map<List<CompanyQueryViewModel>>(companyResponse.Data));
+                }
+                ViewBag.Errors = "Dados não encontrados.";
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(Startup.UrlBase + "CompanyAPI/Detail/?id=" + id);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    string companyJsonResponse = await response.Content.ReadAsStringAsync();
+                    SingleResponse<Company> companyResponse = JsonConvert.DeserializeObject<SingleResponse<Company>>(companyJsonResponse);
+                    return View(_mapper.Map<CompanyQueryViewModel>(companyResponse.Data));
+                }
+                ViewBag.Errors = "Dados não encontrados.";
+                return View();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PresentationLayerMVC/Models/SupplierModels/SupplierCpfQueryViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PresentationLayerMVC.Models.SupplierModels
+{
+    public class SupplierCpfQueryViewModel
+    {
+        public int ID { get; set; }
+
+        [DisplayName("Nome Responsavel")]
+        public string PersonResponsible { get; set; }
+
+        [DisplayName("CPF")]
+        public string CNPJ_CPF { get; set; }
+
+        [DisplayName("Telefone")]
+        public string Telephone { get; set; }
+
+        [DisplayName("Ativo")]
+        public bool Active { get; set; }
+
+        [DisplayName("Empresas")]
+        public virtual ICollection<Company> Companies { get; set; }
+    }
+}
