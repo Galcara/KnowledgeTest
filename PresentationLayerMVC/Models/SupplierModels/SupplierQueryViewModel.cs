ee881250732cefff15fd9b42c5a4ed1c2ea30d74
Bruno Felipe Alves <bruno.felipe_alves@hotmail.com>
2021-03-17 20:12:27 -0300

    up

--- PresentationLayerMVC/Models/SupplierModels/SupplierQueryViewModel.cs
+++ PresentationLayerMVC/Models/SupplierModels/SupplierQueryViewModel.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 
 namespace PresentationLayerMVC.Models.SupplierModels
 {
-    public class SupplierCpfQueryViewModel
+    public class SupplierQueryViewModel
     {
         public int ID { get; set; }
 
         [DisplayName("Nome Responsavel")]
         public string PersonResponsible { get; set; }
 
-        [DisplayName("CPF")]
+        [DisplayName("CPF/CNPJ")]
         public string CNPJ_CPF { get; set; }
 
         [DisplayName("Telefone")]
